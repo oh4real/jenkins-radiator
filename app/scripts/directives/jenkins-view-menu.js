1097bc5b4b94a612533e39b52d5291528ea9a5fd
david byrd <david.byrd@bancvue.com>
2014-06-09 19:29:29 -0500

    refactoring directive so ngmin will work properly

--- app/scripts/directives/jenkins-view-menu.js
+++ app/scripts/directives/jenkins-view-menu.js
@@ -9,25 +9,25 @@
  */
 angular.module('bvRadiatorApp')
 	.directive('jenkinsViewMenu', function() {
-		var directiveDefinitionObject = {};
-		directiveDefinitionObject.templateUrl = 'partials/jenkins-view-menu.html';
-		directiveDefinitionObject.controller = ['$scope', '$http', '$location', function($scope, $http, $location) {
-			$scope.views = [];
-			$http.get('/api/json').success(function(response) {
-				angular.forEach(response.views, function(view) {
-					var project = view.url.match(/\/view\/(.*)\/$/);
-					if (project) {
-						view.project = project[1].replace('%20', ' ');
-						$scope.views.push(view);
-					}
+		return {
+			templateUrl: 'partials/jenkins-view-menu.html',
+			controller: function($scope, $http, $location) {
+				$scope.views = [];
+				$http.get('/api/json').success(function(response) {
+					angular.forEach(response.views, function(view) {
+						var project = view.url.match(/\/view\/(.*)\/$/);
+						if (project) {
+							view.project = project[1].replace('%20', ' ');
+							$scope.views.push(view);
+						}
+					});
 				});
-			});
-			
-			$scope.goToView = function() {
-				var view = '/radiator/' + $scope.selectedView.project;
-				$location.path(view);
-			};
-		}];
-		directiveDefinitionObject.scope = {};
-		return directiveDefinitionObject;
+				
+				$scope.goToView = function() {
+					var view = '/radiator/' + $scope.selectedView.project;
+					$location.path(view);
+				};
+			},
+			scope: {}
+		};
 	});
