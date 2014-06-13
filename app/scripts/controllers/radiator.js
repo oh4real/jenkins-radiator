27ae8a0beac688f5e47bd56d6de398212b86e69e
david byrd <david.byrd@bancvue.com>
2014-06-12 21:08:56 -0500

    oops on styles

--- app/scripts/controllers/radiator.js
+++ app/scripts/controllers/radiator.js
@@ -24,13 +24,20 @@ angular.module('bvRadiatorApp')
 			}, 5000);
 		}
 	};
+	
+	var getPercentCompletion = function(build) {
+		var now = Date.now();
+		var timestamp = build.timestamp;
+		var estimatedDuration = build.estimatedDuration;
+		return 100*(now - timestamp)/estimatedDuration < 100 ? 100*(now - timestamp)/estimatedDuration : 100;
+	};
+
 	var getProjectData = function() {
 		var url = '/view/:project/api/json?depth=2';
 		if (!$routeParams.project) {
 			return;
 		}
 		$http.get(url.replace(':project', $routeParams.project), {cache:false}).success(function(response) {
-			console.log(response);
 			var jobs = {stableJobs:[], failedJobs:[], disabledJobs:[], unknownJobs:[]};
 			angular.forEach(response.jobs, function(job) {
 				if (job.color.indexOf('blue') === 0 || job.color.indexOf('yellow') === 0) {
@@ -42,6 +49,13 @@ angular.module('bvRadiatorApp')
 					jobs.disabledJobs.push(job);
 				} else {
 					jobs.unknownJobs.push(job);
+				}
+				if (job.color.indexOf('_anime') > 1) {
+					job.inprogress = true;
+					job.percentCompleted = getPercentCompletion(job.builds[0]);
+				} else {
+					job.inprogress = false;
+					job.percentCompleted = '';
 				}
 			});
 			$scope.jobs = jobs;
